0181a02b8af0fb80280b3e4f13dc1b0efd0862ee
sha7hid <shahidshafadshahad@gmail.com>
2022-10-02 12:45:22 +0530

    updt

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,6 +3,7 @@ import Cookies from 'universal-cookie';
 import {ChannelList, useChatContext} from 'stream-chat-react';
 import {ChannelSearch, TeamChannelList, TeamChannelPreview} from './';
 import HospitalIcon from '../assets/hospital.png';
+import LogoutIcon from '../assets/logout.png';
 const SideBar = () => (
     <div className="channel-list__sidebar">
 <div className="channel-list__sidebar__icon1">
@@ -10,14 +11,19 @@ const SideBar = () => (
         <img src={HospitalIcon} alt="Hospital" width="30"/>
     </div>
 </div>
+<div className="channel-list__sidebar__icon2">
+    <div className="icon1__inner">
+        <img src={LogoutIcon} alt="Logout" width="30"/>
+    </div>
+</div>
     </div>
 )
 
 function ChannelListContainer() {
   return (
-    <div>
-      ChannelListContainer
-    </div>
+   <>
+   <SideBar/>
+   </>
   )
 }
 
