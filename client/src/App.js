de6d02cf91febc649434e3527c598887e52e47ba
sha7hid <shahidshafadshahad@gmail.com>
2022-10-04 19:55:59 +0530

    updt

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import {StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import {ChannelContainer,ChannelListContainer} from './components';
+import {ChannelContainer,ChannelListContainer,Auth} from './components';
 
 const apiKey = 'rxj4n4w59bfz';
 const client = StreamChat.getInstance(apiKey);
+const authToken = false;
+
 function App() {
+  if(authToken) return <Auth/>
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
