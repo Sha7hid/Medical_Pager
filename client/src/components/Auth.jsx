1ed3e6f939f6fd37dbb357e6e29f3f5fb934d3fd
sha7hid <shahidshafadshahad@gmail.com>
2022-10-05 11:32:45 +0530

    updt

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -3,12 +3,12 @@ import Cookies from 'universal-cookie';
 import axios from 'axios';
 import signinImage from '../assets/assets/signup.jpg'
 const Auth = () => {
-    
+    const [isSignup, setIsSignup] = useState(false);
   return (
     <div className="auth__form-container">
       <div className="auth__form-container_fields">
         <div className="auth__form-container_fields-content">
-            <p>{}</p>
+            <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
         </div>
       </div>
     </div>
