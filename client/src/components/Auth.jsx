078405c9f7a41d78860058e0ad2d6dae27c58d16
sha7hid <shahidshafadshahad@gmail.com>
2022-10-05 12:08:42 +0530

    updt

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -68,6 +68,14 @@ const Auth = () => {
     </div>
         )}
             </form>
+            <div className="auth__form-container_fields-account">
+<p>
+    {isSignup ? "Already have an account?":"Don't have an account"}
+    <span onClick={switchMode}>
+{isSignup ? 'Sign In':'Sign Up'}
+    </span>
+</p>
+            </div>
         </div>
       </div>
     </div>
