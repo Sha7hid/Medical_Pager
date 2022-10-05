06a2ae2c86f8e698db5b684419a54b79313c79b4
sha7hid <shahidshafadshahad@gmail.com>
2022-10-05 17:58:37 +0530

    updt

--- server/index.js
+++ server/index.js
@@ -5,3 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+res.send('Hello World!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
