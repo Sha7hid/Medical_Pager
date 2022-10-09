961314035e83aae48feadff90f635cb60ab8d873
sha7hid <shahidshafadshahad@gmail.com>
2022-10-09 11:25:02 +0530

    updt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,10 +1,17 @@
+const {connect} = require('getstream');
+const bcrypt = require('bcrypt');
+const StreamChat = require('stream-chat');
+const crypto = require('crypto');
+
 const { UserBindingContext } = require("twilio/lib/rest/chat/v2/service/user/userBinding");
 
 const signup = (req, res) => {
     try {
         const { fullName, username, password, phoneNumber} = req.body;
 
-       
+       const userId = crypto.randomBytes(16).toString('hex');
+
+       const serverClient = connect(api_key, api_secret, app_id);
     } catch (error) {
         console.log(error);
 res.status(500).json({message:error});
