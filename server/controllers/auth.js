94e53fc08dddb931f5eb3c7275a2c16fb1b08ed6
sha7hid <shahidshafadshahad@gmail.com>
2022-10-09 11:31:24 +0530

    updt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,6 +5,9 @@ const crypto = require('crypto');
 
 const { UserBindingContext } = require("twilio/lib/rest/chat/v2/service/user/userBinding");
 
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_APP_ID;
 const signup = (req, res) => {
     try {
         const { fullName, username, password, phoneNumber} = req.body;
