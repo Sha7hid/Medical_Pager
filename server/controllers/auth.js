a9dd79a8d9bccc868fea872642ccf724d784be93
sha7hid <shahidshafadshahad@gmail.com>
2022-10-09 11:40:56 +0530

    updt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,13 +8,17 @@ const { UserBindingContext } = require("twilio/lib/rest/chat/v2/service/user/use
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
-const signup = (req, res) => {
+const signup = async (req, res) => {
     try {
         const { fullName, username, password, phoneNumber} = req.body;
 
        const userId = crypto.randomBytes(16).toString('hex');
 
        const serverClient = connect(api_key, api_secret, app_id);
+  
+       const hashedPassword = await bcrypt.hash(password, 10);
+    
+       const token = serverClient.createUserToken
     } catch (error) {
         console.log(error);
 res.status(500).json({message:error});
