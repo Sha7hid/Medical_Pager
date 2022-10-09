7101bb5eb16b8c6f11fef847f2695cf36b889564
sha7hid <shahidshafadshahad@gmail.com>
2022-10-09 11:06:29 +0530

    update

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,19 @@
-const signup = () => {
+const { UserBindingContext } = require("twilio/lib/rest/chat/v2/service/user/userBinding");
+
+const signup = (req, res) => {
     try {
-        
+        const { fullName, username, password, phoneNumber} = req.body;
+
+       
     } catch (error) {
         console.log(error);
 res.status(500).json({message:error});
     }
 };
 
-const login = () => {};
+const login = (req, res) => {
+
+};
 
 
 module.exports = {signup, login}
