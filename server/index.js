7dc53a68152be45526d15e74b6358ed225b51947
sha7hid <shahidshafadshahad@gmail.com>
2022-10-16 11:55:58 +0530

    UPDT

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
 const authRoutes = require("./routes/auth.js");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-require('dotenv').config();
+
 
 app.use(cors());
 app.use(express.json());
